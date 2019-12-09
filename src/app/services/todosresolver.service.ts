deb61a9d237dabc0d86bd8e31c7729caf67542f2
Pavlos <pavloskoz@hotmail.com>
2019-12-09 10:44:10 +0000

    Heroku deploy * 0.1v

--- src/app/services/todosresolver.service.ts
+++ src/app/services/todosresolver.service.ts
@@ -4,17 +4,22 @@ import {Todo, TodoEntry} from '../models/todo.model';
 import {Observable, of} from 'rxjs';
 import {catchError, map} from 'rxjs/operators';
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosResolverService implements Resolve<Todo[]>  {
 
+  baseUrl = environment.apiUrl;
+
   constructor(private http: HttpClient, private router: Router) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Observable<never> {
     console.log('resolver called');
-    return this.http.get('/api/todos/')
+    console.log('called with ', this.baseUrl);
+    const apiUrl = this.baseUrl + '/api/todos/';
+    return this.http.get(apiUrl)
       .pipe(
         catchError(() => {
           console.log('catch error')
