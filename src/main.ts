65aad81fa5604233c16b3b73c0e86d2a7d3becac
Pavlos <pavloskoz@hotmail.com>
2019-12-17 20:06:03 +0000

    Includ jest config file

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  console.log('is prod env built == ', environment.apiUrl)
   enableProdMode();
 }
 
